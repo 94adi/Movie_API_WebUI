5fb8facf4d49616ee9afb2913f35facb7eef15b0
94adi <adrianrcotuna@gmail.com>
2024-12-21 21:08:46 +0200

    Layout fix

--- Movie.WebUI/Controllers/MovieController.cs
+++ Movie.WebUI/Controllers/MovieController.cs
@@ -1,27 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace Movie.WebUI.Controllers;
 
-namespace Movie.WebUI.Controllers
+public class MovieController(ISender sender) : Controller
 {
-    public class MovieController(ISender sender) : Controller
+    [HttpGet]
+    public IActionResult Index()
     {
-        [HttpGet]
-        public IActionResult Index()
+        return View();
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var query = new GetMovieByIdQuery(id);
+
+        var result = await sender.Send(query);
+
+        if(result == null)
         {
-            return View();
+            return NotFound();
         }
 
-        public async Task<IActionResult> Details(int id)
-        {
-            var query = new GetMovieByIdQuery(id);
-
-            var result = await sender.Send(query);
-
-            if(result == null)
-            {
-                return NotFound();
-            }
-
-            return View(result.Movie);
-        }
+        return View(result.Movie);
     }
 }
